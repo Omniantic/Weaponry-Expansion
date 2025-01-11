e0c22dab498891560ea9cf56d24c7adca362ce88
Omniantic <omniantic@gmail.com>
2025-01-11 20:28:55 +0000

    Initial commit

---
+++ src/main/java/net/omni/weaponryexpansion/WeaponryExpansion.java
@@ -0,0 +1,15 @@
+package net.omni.weaponryexpansion;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WeaponryExpansion implements ModInitializer {
+	public static final String MOD_ID = "weaponry_expansion";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
